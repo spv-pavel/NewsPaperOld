6dfdd57b65a84c431aa35b10fca5c518ead67d8d
Pavel Spichakov <spv-pavel@yandex.ru>
2023-03-18 15:37:06 +0300

    Model Author

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,36 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+type_post = [
+    'статья',
+    'новость',
+]
 
 
 class Author(models.Model):
     # cвязь «один к одному» с встроенной моделью пользователей User
+    connection_user = models.OneToOneField(User, on_delete=models.CASCADE)
     # рейтинг пользователя. Ниже будет дано описание того, как этот рейтинг можно посчитать
-    pass
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=25, unique=True)
+# class Category(models.Model):
+#     name = models.CharField(max_length=25, unique=True)
 
 
-class Post(models.Model):
-    # связь один ко многим с Author
-    # поле с выбором типа, статья или новость
-    data_create = models.DateField(auto_now_add=True)  # автоматически добавляемая дата и время создания
-    # связь «многие ко многим» с моделью Category (с дополнительной моделью PostCategory)
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    # рейтинг статьи/новости
-
-
-class PostCategory(models.Model):
-    # связь «один ко многим» с моделью Post
-    # связь «один ко многим» с моделью Category
-    pass
-
-
-class Comment(models.Model):
-    # связь «один ко многим» с моделью Post
-    # связь «один ко многим» со встроенной моделью User (комментарии может оставить любой пользователь, необязательно автор)
-    text = models.CharField(max_length=255)
-    date_create = models.DateField(auto_now_add=True)
-    # рейтинг комментария
-    pass
+# class Post(models.Model):
+#     # связь один ко многим с Author
+#     connection_author = models.ForeignKey(Author, on_delete=models.CASCADE)
+#     # поле с выбором типа, статья или новость
+#     type_ = models.CharField(choices=type_post, default=type_post[0])
+#     data_create = models.DateField(auto_now_add=True)  # автоматически добавляемая дата и время создания
+#     # связь «многие ко многим» с моделью Category (с дополнительной моделью PostCategory)
+#     connection_category = models.ManyToManyField(Category)
+#     title = models.CharField(max_length=255)
+#     text = models.TextField()
+#     # рейтинг статьи/новости
+#
+#
+# class PostCategory(models.Model):
+#     # связь «один ко многим» с моделью Post
+#     connection_post = models.OneToOneField(Post, on_delete=models.CASCADE)
+#     # связь «один ко многим» с моделью Category
+#     connection_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#
+#
+# class Comment(models.Model):
+#     # связь «один ко многим» с моделью Post
+#     connection_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     # связь «один ко многим» со встр моделью User (комментарии может оставить любой пользователь, необязательно автор)
+#     connection_user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     text = models.CharField(max_length=255)
+#     date_create = models.DateField(auto_now_add=True)
+#     # рейтинг комментария
