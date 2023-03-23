3be69bfeba5349d8e6b946cb97c4a54d33b2de97
Pavel Spichakov <spv-pavel@yandex.ru>
2023-03-23 12:49:00 +0300

    models

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -17,42 +17,67 @@ class Author(models.Model):
         # суммарный рейтинг каждой статьи автора умножается на 3
         # суммарный рейтинг всех комментариев автора;
         # суммарный рейтинг всех комментариев к статьям автора
-        # author_pst_rating = self.post_set.all().aggregate(post_rating=Sum('post_rating'))['post_rating'] * 3
-        # author_pst_rating = Post.objects.filter(user_author=self).aggregate(Sum('rating_news')).get('rating_news__sum') * 3
-        author_pst_rating = Post.objects.filter(Author=self).aaggregate(Sum('post_rating')) * 3
-        author_rating_of_comm = Comment.objects.filter(Author=self).aaggregate(Sum('text'))
-        author_rating_to_comm = Comment.objects.filter(Post=self).aaggregate(Sum('text'))
-        self.author_rate = author_pst_rating + author_rating_of_comm + author_rating_to_comm
+        # rating_posts_author = Post.objects.filter(author_id=self.pk).aaggregate(rating=Sum('post_rating'))['post_rating'] * 3  # noqa: E501
+        # rating_comments_author = Comment.objects.filter(user_id=self.user).aaggregate(comment_rating=Sum('comment_rating'))['comment_rating']  # noqa: E501
+        # rating_comments_posts = Comment.objects.filter(post__author__user=self.user).aaggregate(comment_rating=Sum('comment_rating'))['comment_rating']  # noqa: E501
+        rating_posts_author = Post.objects.filter(author_id=self.pk).aaggregate(Sum('post_rating'))['post_rating'] * 3  # noqa: E501
+        rating_comments_author = Comment.objects.filter(user_id=self.user).aaggregate(Sum('comment_rating'))['comment_rating']  # noqa: E501
+        rating_comments_posts = Comment.objects.filter(post__author__user=self.user).aaggregate(Sum('comment_rating'))['comment_rating']  # noqa: E501
+        self.author_rating = rating_posts_author + rating_comments_author + rating_comments_posts  # noqa: E501
         self.save()
-        return self.author_rate
+        return self.author_rating
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=25, unique=True)
+    sport = 'SP'
+    politics = 'PO'
+    education = 'ED'
+    weather = 'WE'
+
+    CATEGORY_TYPES = [
+        (sport, 'Спорт'),
+        (politics, 'Политика'),
+        (education, 'Образование'),
+        (weather, 'Погода'),
+    ]
+
+    category_name = models.CharField(max_length=2,
+                                     choices=CATEGORY_TYPES,
+                                     default=sport,
+                                     unique=True
+                                     )
 
 
 class Post(models.Model):
+    article = 'ARTI'
+    news = 'NEWS'
+
+    POST_TYPE = [
+        (article, 'Статья'),
+        (news, 'Новость')
+    ]
     # связь один ко многим с Author
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    type = models.CharField(max_length=255, choices=TYPE_POST)
+    types = models.CharField(max_length=4, choices=POST_TYPE, default=news)
     data_create = models.DateField(auto_now_add=True)
     # связь «многие ко многим» с моделью Category(с доп. моделью PostCategory)
-    category = models.ManyToManyField(Category)
-    title = models.CharField(max_length=255)
-    text = models.TextField()
+    category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255, blank=False)
+    text = models.TextField(blank=False)
     post_rating = models.IntegerField(default=0)  # рейтинг статьи/новости
 
     def like(self):
         self.post_rating += 1
+        self.save()
 
     def dislike(self):
         self.post_rating -= 1
+        self.save()
 
     def preview(self):
         # который возвращает начало статьи (предварительный просмотр) длиной
         # 124 символа и добавляет многоточие в конце.
-        preview_text = self.text[:124]
-        return preview_text + '...'
+        return self.text[:124] + '...'
 
 
 class PostCategory(models.Model):
@@ -68,12 +93,14 @@ class Comment(models.Model):
     # связь «один ко многим» со встр моделью User (комментарии может
     #  оставить любой пользователь, необязательно автор)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.CharField(max_length=255)
+    text = models.CharField(max_length=255, blank=False)
     date_create = models.DateField(auto_now_add=True)
     comment_rating = models.IntegerField(default=0)  # рейтинг комментария
 
     def like(self):
         self.comment_rating += 1
+        self.save()
 
     def dislike(self):
         self.comment_rating -= 1
+        self.save()
