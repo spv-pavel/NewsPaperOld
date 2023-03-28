14fc75ebacd2ed149123afa086ea3c4a83085474
Pavel Spichakov <spv-pavel@yandex.ru>
2023-03-28 14:17:21 +0300

    D6.3

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -6,7 +6,7 @@ from .models import Post
 class NewsList(ListView):
     # model = Post
     queryset = Post.objects.filter(types='NEWS')
-    ordering = 'data_create'
+    ordering = '-data_create'
     template_name = 'news.html'
     context_object_name = 'news'
 
